37a9e27b6c67035c05c6a275cdabc7f01da8ce7e
RoNexRobotics <ronexrobotics@gmail.com>
2023-01-10 20:04:56 -0800

    Limelight following stuff
    Needs Limelight filtering changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,14 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -20,6 +27,23 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
+  NetworkTableInstance inst;
+  NetworkTable table;
+  NetworkTableEntry tx;
+  NetworkTableEntry ty;
+  NetworkTableEntry ta;
+
+  DifferentialDrive m_driveTrain;
+  MotorControllerGroup m_leftDrive;
+  MotorControllerGroup m_rightDrive;
+
+  WPI_VictorSPX m_leftFrontMotor;
+  WPI_VictorSPX m_leftBackMotor;
+  WPI_VictorSPX m_rightFrontMotor;
+  WPI_VictorSPX m_rightBackMotor;
+
+  double power = 0;
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -29,6 +53,22 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+    inst = NetworkTableInstance.getDefault();
+    table = inst.getTable("limelight");
+    tx = table.getEntry("tx");
+    ty = table.getEntry("ty");
+    ta = table.getEntry("ta");
+
+    m_leftFrontMotor = new WPI_VictorSPX(22);
+    m_leftBackMotor = new WPI_VictorSPX(23);
+    m_leftDrive = new MotorControllerGroup(m_leftFrontMotor, m_leftBackMotor);
+    m_rightFrontMotor = new WPI_VictorSPX(20);
+    m_rightBackMotor = new WPI_VictorSPX(21);
+    m_rightDrive = new MotorControllerGroup(m_rightFrontMotor, m_rightBackMotor);
+    m_driveTrain = new DifferentialDrive(m_leftDrive, m_rightDrive);
+    m_driveTrain.setSafetyEnabled(false);
+    m_leftDrive.setInverted(true);
   }
 
   /**
@@ -78,7 +118,29 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    double x = Math.round(tx.getDouble(0.0));
+    double y = Math.round(ty.getDouble(0.0));
+    double area = Math.round(ta.getDouble(0.0));
+
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
+
+    if (area < 15) {
+      power = 0.4;
+    } else {
+      power = 0;
+    }
+
+    if (x > 1) {
+      m_driveTrain.arcadeDrive(power*-1, -0.6);
+    } else if (x < -1) {
+      m_driveTrain.arcadeDrive(power*-1, 0.6);
+    } else {
+      m_driveTrain.arcadeDrive(power*-1, 0);
+    }
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
