b033bbe55edf7223096a63c5dd530c14497325b7
SirFire <josiah.werner@gmail.com>
2023-02-12 14:22:10 -0800

    Add more joystick compatibility

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -38,8 +38,8 @@ public class RobotContainer {
   private final DriveSubsystem m_robotDrive = new DriveSubsystem();
 
   // Driver controller
-  XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
-  // Joystick m_driverController = new Joystick(OIConstants.kDriverControllerPort);
+  // XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
+  Joystick m_driverController = new Joystick(OIConstants.kDriverControllerPort);
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -50,18 +50,26 @@ public class RobotContainer {
 
     // Configure default commands
     m_robotDrive.setDefaultCommand(
-      /*
-       * For XboxController, the left stick controls translation of the robot. Turning is controlled by the X axis of the right stick.
-       * 
-       * For Joystick, the controls are self-explanitory.
-       */
+      // For XboxController, the left stick controls translation of the robot. Turning is controlled by the X axis of the right stick.
+      // new RunCommand(
+      //   () -> m_robotDrive.drive(
+      //     -MathUtil.applyDeadband(m_driverController.getLeftY(),
+      //     OIConstants.kDriveDeadband),
+      //     -MathUtil.applyDeadband(m_driverController.getLeftX(),
+      //     OIConstants.kDriveDeadband),
+      //     -MathUtil.applyDeadband(m_driverController.getRightX(),
+      //     OIConstants.kDriveDeadband),
+      //     true, true),
+      // m_robotDrive));
+
+      // For Joystick, the controls are self-explanitory.
       new RunCommand(
         () -> m_robotDrive.drive(
-          -MathUtil.applyDeadband(m_driverController.getLeftY(),
+          -MathUtil.applyDeadband(m_driverController.getY(),
           OIConstants.kDriveDeadband),
-          -MathUtil.applyDeadband(m_driverController.getLeftX(),
+          -MathUtil.applyDeadband(m_driverController.getX(),
           OIConstants.kDriveDeadband),
-          -MathUtil.applyDeadband(m_driverController.getRightX(),
+          -MathUtil.applyDeadband(m_driverController.getZ(),
           OIConstants.kDriveDeadband),
           true, true),
       m_robotDrive));
@@ -74,7 +82,12 @@ public class RobotContainer {
    * and then calling passing it to a {@link JoystickButton}.
    */
   private void configureButtonBindings() {
-    new JoystickButton(m_driverController, Button.kR1.value)
+    // new JoystickButton(m_driverController, Button.kR1.value)
+    //   .whileTrue(new RunCommand(
+    //     () -> m_robotDrive.setX(),
+    //     m_robotDrive));
+    
+    new JoystickButton(m_driverController, 7)
       .whileTrue(new RunCommand(
         () -> m_robotDrive.setX(),
         m_robotDrive));
--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 
@@ -15,8 +17,8 @@ public class SwerveModule {
   private final CANSparkMax m_turningSparkMax;
 
   private final RelativeEncoder m_drivingEncoder;
-  private final RelativeEncoder m_turningEncoder;
-  // private final AbsoluteEncoder m_turningEncoder;
+  // private final RelativeEncoder m_turningEncoder;
+  private final AbsoluteEncoder m_turningEncoder;
 
   private final SparkMaxPIDController m_drivingPIDController;
   private final SparkMaxPIDController m_turningPIDController;
@@ -34,8 +36,8 @@ public class SwerveModule {
 
     // Setup encoders and PID controllers for the motor controllers.
     m_drivingEncoder = m_drivingSparkMax.getEncoder();
-    m_turningEncoder = m_turningSparkMax.getEncoder();
-    // m_turningEncoder = m_turningSparkMax.getAbsoluteEncoder(Type.kDutyCycle);
+    // m_turningEncoder = m_turningSparkMax.getEncoder();
+    m_turningEncoder = m_turningSparkMax.getAbsoluteEncoder(Type.kDutyCycle);
     m_drivingPIDController = m_drivingSparkMax.getPIDController();
     m_turningPIDController = m_turningSparkMax.getPIDController();
     m_drivingPIDController.setFeedbackDevice(m_drivingEncoder);
