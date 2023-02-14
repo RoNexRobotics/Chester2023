575471afb458bdb96f312710e5b9fe72ad46bcfb
SirFire <josiah.werner@gmail.com>
2023-02-13 16:17:48 -0800

    Add TODO comment

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -50,6 +50,7 @@ public class RobotContainer {
 
     // Configure default commands
     m_robotDrive.setDefaultCommand(
+      // TODO: Ensure controller inputs are correct.
       // For XboxController, the left stick controls translation of the robot. Turning is controlled by the X axis of the right stick.
       // new RunCommand(
       //   () -> m_robotDrive.drive(
